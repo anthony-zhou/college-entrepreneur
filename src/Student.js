327e7a0e195e09ebf01fa3fda273a877838b4a36
wbne <firetruckbw@gmail.com>
2019-11-09 20:36:44 -0600

    Fixed some potato mistakes and added interest rates

--- src/Student.js
+++ src/Student.js
@@ -4,16 +4,18 @@ class Student {
     this.accountBalance = 0;
     this.semesterTuitionRate = 8500;
     this.studentLoan = -8500;
+    this.APR = 0.05;
+    this.SLIRate = 0.01;
   }
   addMoney(val){
     this.accountBalance += val;
     if(this.accountBalance < 0){
       creditScore -= 10;
     }
+  }
   payLoan(val){
     this.studentLoan += val;
     this.accountBalance -= val;
-  }
   }
 }
 
