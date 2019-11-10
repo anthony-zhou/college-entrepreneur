95bc0544eca78f2d9947a6c36e6f4d81f096358c
anthony-zhou <anthony.zhou03@gmail.com>
2019-11-09 20:53:18 -0600

    Created Dashboard.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+import Dashboard from "./Dashboard";
 import Student from "./Student";
-import World from "./World";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -39,42 +34,7 @@ function App() {
   const student = new Student();
   //const world = new World();
   return (
-    <div className="App">
-      <div className="container">
-        <Grid container justify="center" className={classes.container}>
-          <Grid item xs>
-            <Paper className={`paper ${ classes.paper }`}>Credit Score: <p>{student.creditScore}</p></Paper>
-          </Grid>
-          <Grid item xs>
-            <Paper className={`paper ${ classes.paper }`}>Account Balance: <p>${student.accountBalance}</p></Paper>
-          </Grid>
-          <Grid item xs>
-            <Paper className={`paper ${ classes.paper }`}>Yearly Tuition Rate: <p>${student.yearlyTuitionRate}</p></Paper>
-          </Grid>
-          <Grid item xs>
-            <Paper className={`paper ${ classes.paper }`}>
-              Date:
-              <World event="test"></World>
-              <Button variant="contained" color="primary">Next Month</Button>
-            </Paper>
-          </Grid>
-          <GridList className={classes.feed} cellHeight={160} cols={1}>
-            <GridListTile>
-            <Paper className={classes.newsItem}>
-              <h2>Hello</h2>
-              <p>Event Description</p>
-            </Paper>
-            </GridListTile>
-            <GridListTile>
-              <Paper className={classes.newsItem}>
-              <h2>Hello</h2>
-              <p>Event Description</p>
-              </Paper>
-            </GridListTile>
-          </GridList>
-        </Grid>
-      </div>
-    </div>
+    <Dashboard classes={classes} student={student}></Dashboard>
   );
 }
 
