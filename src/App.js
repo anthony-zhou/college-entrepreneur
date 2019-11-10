2bf0ab0cb58eec005e3621a8037a4f4bdd7f432a
anthony-zhou <anthony.zhou03@gmail.com>
2019-11-09 20:35:20 -0600

    Update Dashboard Formatting in App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import logo from './logo.svg';
 import './App.css';
 import Student from "./Student";
 import World from "./World";
 
 const useStyles = makeStyles(theme => ({
   container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
     gridGap: theme.spacing(3)
   },
   paper: {
     padding: theme.spacing(1),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginBottom: theme.spacing(1),
-    height: 140,
-    width: 100
+    textWrap: 'noWrap',
+    color: theme.palette.text.primary,
+    marginBottom: theme.spacing(1)
   },
   divider: {
     margin: theme.spacing(2, 0),
   },
+  feed: {
+    textAlign: 'left'
+  },
+  newsItem: {
+    padding: theme.spacing(1)
+  }
 }));
 
 function App() {
@@ -32,22 +40,38 @@ function App() {
   //const world = new World();
   return (
     <div className="App">
-      <div>
-        <Grid class="container">
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>Credit Score: {student.creditScore}</Paper>
+      <div className="container">
+        <Grid container justify="center" className={classes.container}>
+          <Grid item xs>
+            <Paper className={`paper ${ classes.paper }`}>Credit Score: <p>{student.creditScore}</p></Paper>
           </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>Account Balance: {student.accountBalance}</Paper>
+          <Grid item xs>
+            <Paper className={`paper ${ classes.paper }`}>Account Balance: <p>${student.accountBalance}</p></Paper>
           </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>Yearly Tuition Rate: {student.yearlyTuitionRate}</Paper>
+          <Grid item xs>
+            <Paper className={`paper ${ classes.paper }`}>Yearly Tuition Rate: <p>${student.yearlyTuitionRate}</p></Paper>
           </Grid>
-          <Grid item>
-            <Paper>
+          <Grid item xs>
+            <Paper className={`paper ${ classes.paper }`}>
+              Date:
               <World event="test"></World>
+              <Button variant="contained" color="primary">Next Month</Button>
             </Paper>
           </Grid>
+          <GridList className={classes.feed} cellHeight={160} cols={1}>
+            <GridListTile>
+            <Paper className={classes.newsItem}>
+              <h2>Hello</h2>
+              <p>Event Description</p>
+            </Paper>
+            </GridListTile>
+            <GridListTile>
+              <Paper className={classes.newsItem}>
+              <h2>Hello</h2>
+              <p>Event Description</p>
+              </Paper>
+            </GridListTile>
+          </GridList>
         </Grid>
       </div>
     </div>
