f965339a3cd1f64ec1bebbbfc67975ea83759462
anthony-zhou <anthony.zhou03@gmail.com>
2019-11-09 22:17:15 -0600

    Updates

---
+++ precache-manifest.94fd4b1dcbee0c9ba318b78b4cd90716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8b6040e3fab8660ecc2e96f01ef7eb",
+    "url": "/college-entrepreneur/index.html"
+  },
+  {
+    "revision": "1209d3c6fcc76bb5149c",
+    "url": "/college-entrepreneur/static/css/main.05d3abdc.chunk.css"
+  },
+  {
+    "revision": "e215356fc72f50f41de9",
+    "url": "/college-entrepreneur/static/js/2.a6a159ad.chunk.js"
+  },
+  {
+    "revision": "1209d3c6fcc76bb5149c",
+    "url": "/college-entrepreneur/static/js/main.f82232bb.chunk.js"
+  },
+  {
+    "revision": "85056177d1579bf61f96",
+    "url": "/college-entrepreneur/static/js/runtime-main.9128ed27.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/college-entrepreneur/precache-manifest.94fd4b1dcbee0c9ba318b78b4cd90716.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/college-entrepreneur/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
