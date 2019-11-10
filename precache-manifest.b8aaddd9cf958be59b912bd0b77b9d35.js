bcc8c832d403927daef0fa0e1ae37a789dba399d
anthony-zhou <anthony.zhou03@gmail.com>
2019-11-10 11:55:22 -0600

    Updates

---
+++ precache-manifest.b8aaddd9cf958be59b912bd0b77b9d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fe220ad7567ee975e633fad9a2d832",
+    "url": "/college-entrepreneur/index.html"
+  },
+  {
+    "revision": "0f3eed51a054e9104789",
+    "url": "/college-entrepreneur/static/css/main.1359359f.chunk.css"
+  },
+  {
+    "revision": "73d825e3d00e4af15c78",
+    "url": "/college-entrepreneur/static/js/2.8c5ed23b.chunk.js"
+  },
+  {
+    "revision": "0f3eed51a054e9104789",
+    "url": "/college-entrepreneur/static/js/main.569feef7.chunk.js"
+  },
+  {
+    "revision": "52907458303805fe275f",
+    "url": "/college-entrepreneur/static/js/runtime-main.8bc3fb85.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/college-entrepreneur/static/media/logo.25bf045c.svg"
+  }
+]);
